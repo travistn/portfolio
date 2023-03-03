818cdc9acdd67922b231c5961b0331d7f66b457c
travistn <travisn714@gmail.com>
2023-03-03 15:42:03 -0800

    Added backgroud image

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        bg: 'url(../public/bg.png)',
+      },
       fontFamily: {
         sans: ['var(--font-montserrat)'],
       },
