e7caf95b1076e9da9a6fb934aa82119f259cca32
travistn <travisn714@gmail.com>
2023-03-03 15:43:18 -0800

    Created navbar with website logo

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className='flex flex-row justify-between px-6 py-4'>
+      <h1 className='text-[26px] tracking-[-1px] font-extrabold'>
+        Travistn<span className='text-blue-500'>.</span>dev
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
