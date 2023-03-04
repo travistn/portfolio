47aca8054649ceefd19fdf13599255f08295240e
travistn <travisn714@gmail.com>
2023-03-03 19:28:41 -0800

    Added a dark mode toggle to the navbar and added styling for dark mode

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,12 @@
+import DarkModeToggle from './DarkModeToggle';
+
 const Navbar = () => {
   return (
-    <nav className='flex flex-row justify-between px-6 py-4'>
-      <h1 className='text-[26px] tracking-[-1px] font-extrabold'>
+    <nav className='flex flex-row justify-between items-center px-6 py-4 mx-auto lg:w-[60%]'>
+      <h1 className='text-[26px] tracking-[-1px] font-extrabold lg:text-[30px] dark:text-white'>
         Travistn<span className='text-blue-500'>.</span>dev
       </h1>
+      <DarkModeToggle />
     </nav>
   );
 };
