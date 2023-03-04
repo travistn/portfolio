5a4e84694cdfe1387f19dd22c8f3f4aef456a499
travistn <travisn714@gmail.com>
2023-03-03 18:31:08 -0800

    Added dark mode styling

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className={`${montserrat.variable} font-sans`}>
-      <body className='bg-bg bg-cover min-h-screen'>{children}</body>
+      <body className='bg-bg bg-cover min-h-screen dark:bg-none dark:bg-[#181a1b]'>{children}</body>
     </html>
   );
 }
