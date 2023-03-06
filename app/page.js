3f28e92d09ee0b026eba34071931e296fdc7a773
travistn <travisn714@gmail.com>
2023-03-05 22:48:17 -0800

    Added About Me section to the website

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Navbar from '@/components/Navbar';
+import About from '@/sections/About';
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <main className='flex flex-col items-center px-6 mx-auto lg:w-[60%]'>
+        <About />
+      </main>
     </>
   );
 }
