acc0adb643c948fa5b85e34b923442123c6f41d1
travistn <travisn714@gmail.com>
2023-03-06 20:26:43 -0800

    Added a carousel of used skills to the About section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,7 +1,9 @@
+import SkillsCarousel from '@/components/SkillsCarousel';
+
 const About = () => {
   return (
-    <section className='flex flex-col items-center gap-6 py-16 lg:py-[6rem]'>
-      <h1 className='text-blue-500 uppercase font-bold lg:text-[20px] dark:text-blue-400'>
+    <section className='flex flex-col gap-6 px-6 py-16 lg:mx-auto lg:py-[6rem] lg:w-[60%]'>
+      <h1 className='text-blue-500 uppercase font-bold text-center lg:text-[20px] dark:text-blue-400'>
         Travis Nguyen
       </h1>
       <h2 className='font-extrabold text-[44px] text-center leading-[50px] lg:text-[60px] lg:leading-[90px] dark:text-white'>
@@ -11,6 +13,9 @@ const About = () => {
         Hi there, I’m Travis! Being a web developer, I am passionate in creating new experiences,
         implementing eye catching designs, and making websites come to life.
       </p>
+      <div className='mt-8'>
+        <SkillsCarousel />
+      </div>
     </section>
   );
 };
