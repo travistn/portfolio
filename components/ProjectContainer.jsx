5f4e4b1e1919bdeb420dfbfda6afc0129ac65a36
travistn <travisn714@gmail.com>
2023-03-09 16:53:42 -0800

    Projects will be displayed in an alternating pattern

--- components/ProjectContainer.jsx
+++ components/ProjectContainer.jsx
@@ -1,11 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { ArrowUpRightIcon } from '@heroicons/react/24/solid';
+import { ArrowUpRightIcon } from '@heroicons/react/24/outline';
 
-const ProjectContainer = ({ project }) => {
+const ProjectContainer = ({ project, index }) => {
   return (
     <article className='relative grid lg:grid-cols-11'>
-      <div className='max-w-[320px] relative justify-self-center z-[-2] lg:justify-self-auto lg:row-span-full lg:col-start-1 lg:col-span-6 lg:max-w-[440px] xl:max-w-[480px]'>
+      <div
+        className={`max-w-[320px] relative justify-self-center z-[-2] lg:row-span-full lg:max-w-[440px] xl:max-w-[480px] ${
+          index % 2 === 0
+            ? 'lg:col-start-1 lg:col-span-6 lg:justify-self-auto'
+            : 'lg:col-start-7 lg:col-span-6 lg:justify-self-end'
+        }`}>
         <Image
           src={project.image}
           alt='website'
@@ -14,8 +19,11 @@ const ProjectContainer = ({ project }) => {
           className='brightness-[0.8]'
         />
       </div>
-      <div className='flex flex-col gap-4 p-8 relative bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] lg:row-span-full lg:col-start-5 lg:col-end-12 lg:self-center'>
-        <h4 className='bg-[#f9f366] py-2 px-8 text-center relative top-[-4.5rem] z-[-1] font-semibold text-[20px] lg:max-w-max'>
+      <div
+        className={`flex flex-col gap-4 p-8 bg-white relative shadow-[0_3px_10px_rgb(0,0,0,0.2)] lg:row-span-full lg:self-center ${
+          index % 2 === 0 ? 'lg:col-start-5 lg:col-end-12' : 'lg:col-start-1 lg:col-end-8'
+        } `}>
+        <h4 className='bg-[#f9f366] py-2 px-8 text-center relative top-[-4.5rem] z-[-1] font-semibold text-[20px] lg:max-w-max even:lg:col-start-1 even:lg:col-span-6 '>
           {project.name}
         </h4>
         <p className='mt-[-4.5rem] font-medium text-[17px]'>{project.description1}</p>
@@ -26,14 +34,14 @@ const ProjectContainer = ({ project }) => {
         <div className='flex flex-row justify-between mt-4 text-[18px] font-medium lg:text-[19px] lg:justify-end lg:gap-6'>
           <Link href={project.liveUrl} target='_blank' className='flex flex-row items-center gap-2'>
             Live Site
-            <ArrowUpRightIcon className='w-[19px] h-[19px] transition-transform duration-200 ease-in hover:rotate-45' />
+            <ArrowUpRightIcon className='w-[19px] h-[19px] stroke-2 transition-transform duration-200 ease-in hover:rotate-45' />
           </Link>
           <Link
             href={project.githubUrl}
             target='_blank'
             className='flex flex-row items-center gap-2'>
             Github
-            <ArrowUpRightIcon className='w-[19px] h-[19px] transition-transform duration-200 ease-in hover:rotate-45' />
+            <ArrowUpRightIcon className='w-[19px] h-[19px] stroke-2 transition-transform duration-200 ease-in hover:rotate-45' />
           </Link>
         </div>
       </div>
