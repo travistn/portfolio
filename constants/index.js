7bdad6ceee930bdbc181bc154bb97b8e6534241d
travistn <travisn714@gmail.com>
2023-03-12 21:34:12 -0700

    Created contacts array with data for contact links

--- constants/index.js
+++ constants/index.js
@@ -87,3 +87,16 @@ export const projects = [
     image: '/filmazon.png',
   },
 ];
+
+export const contacts = [
+  {
+    name: 'Github',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg',
+    link: 'https://github.com/travistn',
+  },
+  {
+    name: 'LinkedIn',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg',
+    link: 'https://www.linkedin.com/in/travisn714/',
+  },
+];
