50d71dbed6902d2d55a7b742de3bade9924b2615
travistn <travisn714@gmail.com>
2023-03-22 13:24:43 -0700

    Removed skills array

--- constants/index.js
+++ constants/index.js
@@ -1,46 +1,3 @@
-export const skills = [
-  {
-    name: 'react',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
-  },
-  {
-    name: 'html',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg',
-  },
-  {
-    name: 'css',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg',
-  },
-  {
-    name: 'javascript',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg',
-  },
-  {
-    name: 'typescript',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
-  },
-  {
-    name: 'tailwind',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
-  },
-  {
-    name: 'nextjs',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-line.svg',
-  },
-  {
-    name: 'git',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
-  },
-  {
-    name: 'nodejs',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-plain.svg',
-  },
-  {
-    name: 'npm',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg',
-  },
-];
-
 export const contacts = [
   {
     name: 'Github',
@@ -50,6 +7,6 @@ export const contacts = [
   {
     name: 'LinkedIn',
     icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg',
-    link: 'https://www.linkedin.com/in/travisn714/',
+    link: 'https://www.linkedin.com/in/travistn/',
   },
 ];
