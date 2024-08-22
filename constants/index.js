eaca0da18ea0a053be4d54dd580d97645f02b791
travistn <travisn714@gmail.com>
2024-08-22 16:10:34 -0700

    Replace broken link for Tailwind CSS icon

--- constants/index.js
+++ constants/index.js
@@ -34,7 +34,7 @@ export const skills = [
   },
   {
     name: 'tailwind',
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg',
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg',
   },
   {
     name: 'git',
